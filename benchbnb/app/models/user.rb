10f90a038dabcb2e1931f853c695d6ca6049d3c4
sean <midruss@yahoo.com>
2022-02-22 15:21:23 -0500

    Started backend user auth methods

--- benchbnb/app/models/user.rb
+++ benchbnb/app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+    #FIGVAPER
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def is_password?(password)
+        Bcrypt::
+    end
 end
