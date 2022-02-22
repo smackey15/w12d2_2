e0aa213dfafe18bde719729e6044b41373872f2c
YWBK <ywbkdev@gmail.com>
2022-02-22 16:47:12 -0500

    Sessions#create

--- benchbnb/app/controllers/api/sessions_controller.rb
+++ benchbnb/app/controllers/api/sessions_controller.rb
@@ -1,2 +1,18 @@
 class Api::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+        if @user
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+
+    end
 end
