021a80bc3b7edf9411de4675c2a47f84bdf100ec
YWBK <ywbkdev@gmail.com>
2022-02-22 15:48:39 -0500

    backend auth

--- benchbnb/app/models/user.rb
+++ benchbnb/app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-    #FIGVAPER
+    validates :username, :password_digest, presence: true
+    validates :username, uniqueness: true\
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    after_validation :ensure_session_token
+    attr_reader :password
 
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
@@ -10,7 +15,27 @@ class User < ApplicationRecord
         end
     end
 
-    def is_password?(password)
-        Bcrypt::
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64
     end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
+    end
+
 end
