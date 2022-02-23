4a1ad2f6496e1f39862352b01265115aa8ee56ed
sean <midruss@yahoo.com>
2022-02-23 16:12:13 -0500

    Completed Phase 1

--- benchbnb/frontend/bench_bnb.jsx
+++ benchbnb/frontend/bench_bnb.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {signup, login, logout} from "./util/session_api_util"
+import configureStore from "./store/store";
 // import {signup, login, logout} from "./actions/session_actions"
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 
   // TEST //
   window.signup = signup;
   window.login = login;
   window.logout = logout;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   // TEST //
 });
